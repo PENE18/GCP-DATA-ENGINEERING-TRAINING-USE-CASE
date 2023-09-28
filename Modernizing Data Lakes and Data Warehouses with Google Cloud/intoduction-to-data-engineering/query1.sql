2ff8edf727cd4a354b65c04172b09f14f9306555
Mamadou PENE <mamadoupene95@gmail.com>
2023-09-28 21:32:01 +0000

    first-push

---
+++ Modernizing Data Lakes and Data Warehouses with Google Cloud/intoduction-to-data-engineering/query1.sql
@@ -0,0 +1,16 @@
+SELECT
+  MIN(start_station_name) AS start_station_name,
+  MIN(end_station_name) AS end_station_name,
+  APPROX_QUANTILES(tripduration, 10)[OFFSET (5)] AS typical_duration,
+  COUNT(tripduration) AS num_trips
+FROM
+  `bigquery-public-data.new_york_citibike.citibike_trips`
+WHERE
+  start_station_id != end_station_id
+GROUP BY
+  start_station_id,
+  end_station_id
+ORDER BY
+  num_trips DESC
+LIMIT
+  10
